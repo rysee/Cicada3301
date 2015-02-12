4bf15ad9613fe560779817a7640df572909e2c12
RyanAiden <ryanpalestini@Batman.local>
2015-02-12 14:06:33 -0600

    Added multi method

--- AES/AES.java
+++ AES/AES.java
@@ -1,5 +1,10 @@
 class AES {
     public static void main(String[] args) {
-        
+        byte[] test = multiplication(new byte[8], new byte[8]);
+    }
+
+    public static byte[] multiplication(byte[] multiplicand, byte[] multiplier) {
+    	System.out.println("Meow");
+    	return multiplicand;
     }
 }
